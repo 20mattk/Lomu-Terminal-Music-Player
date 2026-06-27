6dd79d3923dd178814202425c55c41c4b5c8e03a
Matt Karenbauer <20mattk@gmail.com>
2026-06-27 15:56:55 -0400

    Adding YYYY-MM-DD validation to Track class.

--- lomu/library/track.py
+++ lomu/library/track.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass, field
 from pathlib import Path
 from enum import Enum
+from datetime import datetime
 
 
 class AudioFormat(str, Enum):
     """
     Represents the only valid audio file formats the application can use.
-
     This Enum is used as a data type for an attribute in the Track class.
     """
     MP3 = ".mp3"
@@ -18,7 +18,7 @@ class AudioFormat(str, Enum):
         """
         Derives the audio format from the file type extension.
         Ensures the extension is of a valid audio format in the class.
-        Hanldes the missing or present leading . in a file type.
+        Hanldes the missing or present leading "." in a file type.
 
         Arguments:
             cls (AudioFormat): The AudioFormat class passed by @classmethod.
@@ -56,7 +56,7 @@ class Track:
         title (str): Title of the track.
         artist (str): The artist that the track belongs to.
         album (str): The album that the track belongs to.
-        year (str): The year in which the track was released.
+        release_date (str): 'YYYY-MM-DD' date on which the album was released.
         track_number (int): The order in which the track appears on its album.
         duration (float): The length in seconds of the track.
     """
@@ -64,7 +64,7 @@ class Track:
     title: str
     artist: str
     album: str
-    year: str
+    release_date: str
     track_number: int
     duration: float
     audio_format: AudioFormat = field(init=False)
@@ -75,6 +75,43 @@ class Track:
 
         object.__setattr__(
             self,
+            "release_date",
+            self._normalize_date(self.release_date)
+        )
+
+        object.__setattr__(
+            self,
             "audio_format",
             AudioFormat.from_suffix(self.file_path.suffix)
+        )
+
+    def _normalize_date(self, date: str) -> str:
+        """
+        Ensures that a provided date string is of a valid format.
+        If the format is valid, the date is transformed into 'YYYY-MM-DD'.
+        Valid inputs: 'YYYY-MM-DD', 'YYYY', 'YY-MM-DD'
+
+        Arguments:
+            self (Track): The instance of the Track class.
+            date (str): The date string to validate and normalize.
+
+        Returns:
+            (str): A string of the valid date formatted to 'YYYY-MM-DD'.
+
+        Raises:
+            ValueError: If the inputted string is of no valid format.
+        """
+        valid_formats: list[str] = ["%Y-%m-%d", "%Y", "%y-%m-%d"]
+
+        for valid_format in valid_formats:
+            try:
+                return datetime.strptime(
+                    date, valid_format
+                ).strftime("%Y-%m-%d")
+            except:
+                pass
+
+        raise ValueError(
+            f"Invalid date format: {date}. "
+            f"Valid formats: {valid_formats}."
         )
