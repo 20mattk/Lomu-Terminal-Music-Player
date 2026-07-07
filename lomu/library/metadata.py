00e9852c879c8eb1620c343790cdc76c540a6851
Matt Karenbauer <20mattk@gmail.com>
2026-07-07 15:57:07 -0400

    Implementing MP3, FLAC, WAV metadata extraction

--- lomu/library/metadata.py
+++ lomu/library/metadata.py
@@ -1,13 +1,8 @@
-# === TODO === #
-# 1. Finish MP3 tag extraction
-# 2. Finish WAV tag extraction
-# 3. Implement album_art image (path or data) extraction
-
-
 from .track import Track, AudioFormat
 from abc import ABC, abstractmethod
 from pathlib import Path
-from mutagen.id3 import ID3
+from typing import Optional
+from mutagen.id3 import ID3, APIC
 from mutagen.flac import FLAC
 from mutagen.wave import WAVE
 from mutagen import File
@@ -31,60 +26,25 @@ class MP3MetadataExtractor(MetadataExtractor):
     Extracts .mp3 file metadata to generate a Track object.
     """
     def extract_metadata(self, file_path: Path) -> Track:
+        """
+        The inherited abstract method which has been defined for .mp3 files.
+        Creates and returns a Track object.
+        """
         audio: ID3 = ID3(file_path)
 
         title: str = audio.get("TIT2", "Unknown Title")
         artist: str = audio.get("TPE1", "Unknown Artist")
         album: str = audio.get("TALB", "Unknown Album")
-        release_date: str = audio.get("TDRL", "Unknown Release Date")
-        track_number: int = int(
-            audio.get("TRCK", "Unknown Track Number").text[0]
+        release_date: str = str(
+            audio.get("TDOR", str(audio.get("TDRC", "0001")))
         )
+        track_number: int = int(audio.get("TRCK", "1").text[0].split("/")[0])
         duration: float = File(file_path).info.length
-        # album_art
-
-        print(
-            f"File Path: {file_path}\n"
-            f"Title: {title}\n"
-            f"Artist: {artist}\n"
-            f"Album: {album}\n"
-            f"Release Date: {release_date}\n"
-            f"Track Number: {track_number}\n"
-            f"Duration: {duration}\n"
-            # f"Album Art: {album_art}\n"
-        )
-
-        # return Track(
-        #     file_path=file_path,
-        #     title="Title",
-        #     artist="Artist",
-        #     album="Album",
-        #     release_date="1907-01-01",
-        #     track_number=1,
-        #     duration=180.00
-        #     album_art=album_art
-        # )
-
-        pass
-
-
-class FLACMetadataExtractor(MetadataExtractor):
-    """
-    Concrete implementation of the MetadataExtractor interface.
-    Extracts .flac file metadata to generate a Track object.
-    """
-    def extract_metadata(self, file_path: Path) -> Track:
-        audio: FLAC = FLAC(file_path)
-
-        title: str = audio.get("title", ["Unknown Title"])[0]
-        artist: str = audio.get("artist", ["Unknown Artist"])[0]
-        album: str = audio.get("album", ["Unknown Album"])[0]
-        release_date: str = audio.get("date", ["Unknown Release Date"])[0]
-        track_number: int = int(
-            audio.get("number", ["Unknown Track Number"])[0]
-        )
-        duration: float = File(file_path).info.length
-        # album_art
+        album_art: Optional[bytes] = None
+        for frame in audio.values():
+            if isinstance(frame, APIC):
+                album_art = frame.data
+                break
 
         return Track(
             file_path=file_path,
@@ -93,8 +53,42 @@ class FLACMetadataExtractor(MetadataExtractor):
             album=album,
             release_date=release_date,
             track_number=track_number,
-            duration=duration
-            # album_art=album_art
+            duration=duration,
+            album_art=album_art
+        )
+
+
+class FLACMetadataExtractor(MetadataExtractor):
+    """
+    Concrete implementation of the MetadataExtractor interface.
+    Extracts .flac file metadata to generate a Track object.
+    """
+    def extract_metadata(self, file_path: Path) -> Track:
+        """
+        The inherited abstract method which has been defined for .flac files.
+        Creates and returns a Track object.
+        """
+        audio: FLAC = FLAC(file_path)
+
+        title: str = audio.get("title", ["Unknown Title"])[0]
+        artist: str = audio.get("artist", ["Unknown Artist"])[0]
+        album: str = audio.get("album", ["Unknown Album"])[0]
+        release_date: str = audio.get("date", ["0001"])[0]
+        track_number: int = int(audio.get("number", ["1"])[0])
+        duration: float = File(file_path).info.length
+        album_art: Optional[bytes] = (
+            audio.pictures[0].data if audio.pictures else None
+        )
+
+        return Track(
+            file_path=file_path,
+            title=title,
+            artist=artist,
+            album=album,
+            release_date=release_date,
+            track_number=track_number,
+            duration=duration,
+            album_art=album_art
         )
 
 
@@ -104,48 +98,33 @@ class WAVMetadataExtractor(MetadataExtractor):
     Extracts .wav file metadata to generate a Track object.
     """
     def extract_metadata(self, file_path: Path) -> Track:
-        # print(WAVE(file_path))
+        """
+        The inherited abstract method which has been defined for .wav files.
+        Creates and returns a Track object.
+        """
         audio: WAVE = WAVE(file_path)
-
-        # title = "TIT2"
-        # artist = "TPE1"
-        # album = "TALB"
-        # release_date = "
-        # track_number = "TRCK"
-        # duration = 
-        # ? album_art = "APIC" ?
 
         title: str = audio.get("TIT2", ["Unknown Title"])[0]
         artist: str = audio.get("TPE1", ["Unknown Artist"])[0]
         album: str = audio.get("TALB", ["Unknown Album"])[0]
-        release_date: str = audio.get("date", ["Unknown Release Date"])[0]
-        track_number: str = int(audio.get("TRCK", ["Unknown Track Number"])[0].split("/")[0])
+        release_date: str = audio.get("TDRC", ["0001"])[0]
+        track_number: str = int(audio.get("TRCK", ["1"])[0].split("/")[0])
         duration: float = File(file_path).info.length
-        # album_art
+        for frame in audio.values():
+            if isinstance(frame, APIC):
+                album_art = frame.data
+                break
 
-        print(
-            f"File Path: {file_path}\n"
-            f"Title: {title}\n"
-            f"Artist: {artist}\n"
-            f"Album: {album}\n"
-            f"Release Date: {release_date}\n"
-            f"Track Number: {track_number}\n"
-            f"Duration: {duration}\n"
-            # album_art
+        return Track(
+            file_path=file_path,
+            title=title,
+            artist=artist,
+            album=album,
+            release_date=release_date,
+            track_number=track_number,
+            duration=duration,
+            album_art=album_art
         )
-
-        # return Track(
-        #     file_path=file_path,
-        #     title="Title",
-        #     artist="Artist",
-        #     album="Album",
-        #     release_date="1907-01-01",
-        #     track_number=1,
-        #     duration=180.00
-        #     album_art
-        # )
-
-        pass
 
 
 class MetadataFactory:
