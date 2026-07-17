79f0688897d9a879b86f2b3e0e59bbdffb4b7455
Matt Karenbauer <20mattk@gmail.com>
2026-07-17 18:13:13 -0400

    Adding playlist module to library

--- lomu/library/__init__.py
+++ lomu/library/__init__.py
@@ -1,9 +1,11 @@
 from .track import Track, AudioFormat
 from .metadata import load_track
+from .playlist import Playlist
 
 
 __all__ = [
     "Track",
     "AudioFormat",
-    "load_track"
+    "load_track",
+    "Playlist"
 ]
