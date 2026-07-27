9ca162710c9ea49676447db1ec8cb707b0a63ff5
Matt Karenbauer <20mattk@gmail.com>
2026-07-27 11:36:20 -0400

    Adding in MusicLibrary as avialable module

--- lomu/library/__init__.py
+++ lomu/library/__init__.py
@@ -1,11 +1,13 @@
 from .track import Track, AudioFormat
 from .metadata import load_track
 from .playlist import Playlist
+from .music_library import MusicLibrary
 
 
 __all__ = [
     "Track",
     "AudioFormat",
     "load_track",
-    "Playlist"
+    "Playlist",
+    "MusicLibrary"
 ]
