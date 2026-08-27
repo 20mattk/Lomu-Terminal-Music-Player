a243caf566c2791a9c417a68d3f697a44b1901e9
Matt Karenbauer <20mattk@gmail.com>
2026-08-27 16:49:47 -0400

    Completing MusicLibrary tests

--- tests/test_music_library.py
+++ tests/test_music_library.py
@@ -4,6 +4,18 @@ from lomu.library import MusicLibrary
 
 
 # === FIXTURES ============================================================== #
+@pytest.fixture
+def test_track_1() -> Track:
+    """Fixture to create a basic Track object"""
+    return Track(
+        file_path=Path("test_track_1.flac"),
+        title="Test Track 1 Title",
+        artist="Test Track 1 Artist",
+        album="Test Track 1 Album",
+        release_date="2024-08-19",
+        track_number=3,
+        duration=201.542
+    )
 
 
 # === TEST CASES ============================================================ #
@@ -33,7 +45,9 @@ class TestLibraryInitialization:
 class TestFileScanning:
     def test_scanning_returns_paths(self):
         """Test: scan_home_dir returns a list of Path objects"""
-        file_paths: list[Path] = MusicLibrary(Path("./Audio/")).scan_home_dir()
+        file_paths: list[Path] = MusicLibrary(
+            Path("./tests/Audio")
+        ).scan_home_dir()
         assert all(isinstance(file, Path) for file in file_paths)
 
     def test_scanning_empty_path(self):
@@ -43,14 +57,38 @@ class TestFileScanning:
 
 
 class TestLibraryPopulation:
-    pass
-    # create mock track, call populate_library, self.tracks contains it now
-    # test invalid format correctly identified with mock track that's "bad"
+    def test_library_population(self):
+        """Test: Adding tracks successfully populates the library"""
+        library: MusicLibrary = MusicLibrary(Path("./tests/Audio"))
+        library.populate_library()
+        assert library.track_count > 0
 
 
 class TestLibraryCleaning:
-    pass
-    # tracks becomes empty
-    # track_count == 0
-    # total_duration == 0.0
-    # home_dir is still populated
+    def test_library_cleans_tracks(self):
+        """Test: Clearing a populated library clears track list"""
+        library: MusicLibrary = MusicLibrary(Path("./tests/Audio"))
+        library.populate_library()
+        library.clear_library()
+        assert library.tracks == []
+
+    def test_library_cleans_track_count(self):
+        """Test: Clearing a populated library resets track count"""
+        library: MusicLibrary = MusicLibrary(Path("./tests/Audio"))
+        library.populate_library()
+        library.clear_library()
+        assert library.track_count == 0
+
+    def test_library_cleans_duration(self):
+        """Test: Clearing a populated library resets total duration"""
+        library: MusicLibrary = MusicLibrary(Path("./tests/Audio"))
+        library.populate_library()
+        library.clear_library()
+        assert library.total_duration == 0.0
+
+    def test_library_cleans_leaves_home_dir(self):
+        """Test: Clearing a populated library doesn't touch home_dir"""
+        library: MusicLibrary = MusicLibrary(Path("./tests/Audio"))
+        library.populate_library()
+        library.clear_library()
+        assert library.home_dir != None and library.home_dir != ""
