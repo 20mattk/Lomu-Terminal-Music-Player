c4290158aabcba4505cfd24c94af74b29754e684
Matt Karenbauer <20mattk@gmail.com>
2026-08-31 10:29:31 -0400

    Adding some pre-dev notes

---
+++ lomu/playback/events.py
@@ -0,0 +1,3 @@
+# communication bridge
+# listens for signals from the player
+# relays playback information between queue, player, etc.
---
+++ lomu/playback/queue.py
@@ -0,0 +1,3 @@
+# sequencing and ordering
+# iterating through a library and building queue/list of what is to be played
+# handles all sorting/filtering of tracks to hand off to player
